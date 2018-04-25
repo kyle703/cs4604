372793f80cb3d21d9e594d73a49bf7c1ebbd47bc
Kyle Thompson <kyle703@vt.edu>
2018-04-24 21:13:11 -0400

    updated queries and page some

--- query1.php
+++ query1.php
@@ -1,54 +1,47 @@
 <?php
 
+    $database = "dbname='young sailors'";
+    $user = "user='young sailors'";
+    $password = "password=123456";
+  $dbconnect = pg_connect ("$database $user $password");
 
+  $title = "Query 1";
+  echo "<h1>Query 1</h1>";
+  echo "<h4>Items dependent on {$_REQUEST['config']}</h4>";
+  
+  $config = $_REQUEST['config'];
 
-//check if admin or guest to determine connect string
-
-$database = "dbname='young sailors'";
-$user = "user='young sailors'";
-$password = "password=123456";
-
-
-$link = pg_connect ("$database $user $password");
-if (!$link)
-{
-    die('Error: Could not connect: ' . pg_last_error());
-}
-
-$query = "SELECT P.name,  count(P.name)
-FROM ps_administered_by PS, vs_administered_by VS, person P
-WHERE P.pid = PS.hid OR P.pid = VS.pid
-GROUP BY P.name;";
-
-$result = pg_query($query) or die('query failed:'.pg_last_error());
-
-$i = 0;
-echo '<html><body><table><tr>';
-while ($i < pg_num_fields($result))
-{
-    $fieldName = pg_field_name($result, $i);
-    echo '<td>' . $fieldName . '</td>';
-    $i = $i + 1;
-}
-echo '</tr>';
-$i = 0;
-
-while ($row = pg_fetch_row($result)) 
-{
-    echo '<tr>';
-    $count = count($row);
-    $y = 0;
-    while ($y < $count)
-    {
-        $c_row = current($row);
-        echo '<td>' . $c_row . '</td>';
-        next($row);
-        $y = $y + 1;
+  //Specify a query for the database.
+  $query = "SELECT C2.*
+            FROM configuration_items C1, configuration_items C2,
+            dependent_on D
+            WHERE C1.name = '{$config}' AND C1.cid = D.super
+            AND C2.cid = D.child;";
+  //Get the result of the query
+  $result = pg_query($dbconnect, $query);
+  
+  //And record the number of fields from the query.
+  $numberOfFields = pg_num_fields($result);
+  
+  //Print the table header with the field names.  Use bold font.
+  echo "<table align=\"center\" class=\"table table-striped table-bordered\">";
+  echo "<tr>";
+    for ($i=0; $i<$numberOfFields; $i++) {
+      $fieldName = pg_field_name($result, $i);
+      $fieldType = pg_field_type($result, $i);
+      echo "<th>{$fieldName} ({$fieldType})</th>";
     }
-    echo '</tr>';
-    $i = $i + 1;
-}
-pg_free_result($result);
-
-echo '</table></body></html>';
+  echo "</tr>";
+  
+  //Then, print all the results of the query.
+  while($array = pg_fetch_array($result, NULL, PGSQL_ASSOC)) {
+    echo '<tr>';
+    foreach($array as $elem) {
+      echo '<td>'.$elem.'</td>';
+    }
+    echo '</tr>'; 
+  }
+  echo '</table>';
+  //Close the database when done.
+  pg_close($dbconnect);
 ?>
